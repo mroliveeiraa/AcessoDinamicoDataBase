3b02e9279c4f4730d90d2237a0ccbb119137cac7
mroliveeiraa <133261097+mroliveeiraa@users.noreply.github.com>
2023-09-25 15:10:24 -0300

    Update DataSourceRoutingService.java

--- V1/DataSourceRoutingService.java
+++ V1/DataSourceRoutingService.java
@@ -3,6 +3,9 @@ import org.springframework.stereotype.Service;
 
 /*
 criar um serviço chamado DataSourceRoutingService que gerenciará qual fonte de dados está ativa com base nas decisões do aspecto.
+
+Este serviço estende AbstractRoutingDataSource e determina a fonte de dados com base em um ThreadLocal, 
+que é configurado pelo aspecto. Ele fornece métodos setReadDataSource() e setWriteDataSource() para definir a fonte de dados apropriada.
   */
 
 @Service
